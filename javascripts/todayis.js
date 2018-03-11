feb046c05dcd8770d5126006205da54a89883666
hoojianliang <hoojianliang@users.noreply.github.com>
2018-03-11 10:42:06 +0800

    Update todayis.js
    add in 2018 event

--- javascripts/todayis.js
+++ javascripts/todayis.js
@@ -19,4 +19,18 @@ sampleData["2017"] = [
 	{date:"2017-12-25",event:[{day:"Christmas"}]}
 ];
 
+sampleData["2018"] = [
+	{date:"2018-01-01",event:[{day:"New Year",description:"Calendar's year count increments by one.",url:"http://www.happy-newyearimages.com/wp-content/uploads/2016/09/Happy-new-year-2017-wallpaper-Images-Full-HD.jpg"},
+							{day:"National Hangover Day",description:"A day that traditionally is spent celebrating to bring in the New Year.",url:""}]},
+	{date:"2018-02-16",event:[{day:"Chinese New Year",description:"First day of the first lunar month ",url:"http://data.chinatravel.com/images/focus/chinese-new-year/chinese-new-year-1200.jpg"}]},
+	{date:"2018-02-17",event:[{day:"Chinese New Year (2nd day)",description:"Second day of the lunar New Year.",url:"http://previews.123rf.com/images/acucucu/acucucu1606/acucucu160600249/58504665-Oriental-Happy-Chinese-New-Year-2017-Year-of-Rooster-Design-Stock-Vector.jpg"}]},
+	{date:"2018-03-02",event:[{day:"Latern Festival",description:"Chinese festival celebrated on the fifteenth day of the first month in the lunisolar Chinese calendar.",url:"http://ilikevents.com/images/share/event/new-taipei-city-pingxi-sky-lantern-festival/pic0.jpg"}]},
+	{date:"2018-05-01",event:[{day:"Labour Day"}]},
+	{date:"2018-05-29",event:[{day:"Vesak Day"}]},
+	{date:"2018-08-09",event:[{day:"Singapore National Day"}]},
+	{date:"2018-09-16",event:[{day:"Malaysia Day"}]},
+	{date:"2018-11-06",event:[{day:"Deepavali"}]},
+	{date:"2018-12-25",event:[{day:"Christmas"}]}
+];
+
 
